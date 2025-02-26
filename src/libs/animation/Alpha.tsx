8020dc285ed7e8535d292ef80950f486980d09c6
Federico Valla <38297526+vallafederico@users.noreply.github.com>
2025-02-26 14:04:49 +0100

    kill animation

--- src/libs/animation/Alpha.tsx
+++ src/libs/animation/Alpha.tsx
@@ -1,36 +1,36 @@
-"use client";
-import { ReactNode, useRef } from "react";
-import { useAnimateInView } from ".";
-import gsap from "gsap";
+// "use client";
+// import { ReactNode, useRef } from "react";
+// import { useAnimateInView } from ".";
+// import gsap from "gsap";
 
-export const Alpha = ({ children }: { children: ReactNode }) => {
-  const anim = {
-    el: useRef(null),
-    a: useRef(null),
-  };
+// export const Alpha = ({ children }: { children: ReactNode }) => {
+//   const anim = {
+//     el: useRef(null),
+//     a: useRef(null),
+//   };
 
-  const { ref } = useAnimateInView((intersecting) => {
-    if (intersecting) {
-      anim.a.current = gsap.to(anim.el.current, {
-        autoAlpha: 1,
-        duration: 1,
-        delay: 0.2,
-      });
-    } else {
-      if (anim.a.value?.kill) anim.a.value.kill();
+//   const { ref } = useAnimateInView((intersecting) => {
+//     if (intersecting) {
+//       anim.a.current = gsap.to(anim.el.current, {
+//         autoAlpha: 1,
+//         duration: 1,
+//         delay: 0.2,
+//       });
+//     } else {
+//       if (anim.a.value?.kill) anim.a.value.kill();
 
-      gsap.set(anim.el.current, {
-        autoAlpha: 0,
-        duration: 1,
-      });
-    }
-  });
+//       gsap.set(anim.el.current, {
+//         autoAlpha: 0,
+//         duration: 1,
+//       });
+//     }
+//   });
 
-  return (
-    <div className="overflow-clip" ref={ref}>
-      <div className="invisible" ref={anim.el}>
-        {children}
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div className="overflow-clip" ref={ref}>
+//       <div className="invisible" ref={anim.el}>
+//         {children}
+//       </div>
+//     </div>
+//   );
+// };
