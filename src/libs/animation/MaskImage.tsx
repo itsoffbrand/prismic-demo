bd581c7fd2debc0d2cdccc331e9b8bf7e5c5a486
Federico Valla <38297526+vallafederico@users.noreply.github.com>
2025-02-26 14:08:16 +0100

    voilà

--- src/libs/animation/MaskImage.tsx
+++ src/libs/animation/MaskImage.tsx
@@ -1,37 +1,37 @@
-"use client";
-import { ReactNode, useRef } from "react";
-import { useAnimateInView } from ".";
-import gsap from "gsap";
+// "use client";
+// import { ReactNode, useRef } from "react";
+// import { useAnimateInView } from ".";
+// import gsap from "gsap";
 
-export const MaskImage = ({
-  children,
-  className,
-}: {
-  children: ReactNode;
-  className: string;
-}) => {
-  const anim = {
-    el: useRef(null),
-    a: useRef(null),
-  };
+// export const MaskImage = ({
+//   children,
+//   className,
+// }: {
+//   children: ReactNode;
+//   className: string;
+// }) => {
+//   const anim = {
+//     el: useRef(null),
+//     a: useRef(null),
+//   };
 
-  const { ref, target } = useAnimateInView((visible) => {
-    if (visible) {
-      if (!target) return;
-      anim.a.value = gsap.to(target, {
-        autoAlpha: 1,
-        delay: 0.2,
-      });
-    } else {
-      if (!target) return;
-      if (anim.a.value?.kill) anim.a.value.kill();
-      gsap.set(target, { autoAlpha: 0 });
-    }
-  });
+//   const { ref, target } = useAnimateInView((visible) => {
+//     if (visible) {
+//       if (!target) return;
+//       anim.a.value = gsap.to(target, {
+//         autoAlpha: 1,
+//         delay: 0.2,
+//       });
+//     } else {
+//       if (!target) return;
+//       if (anim.a.value?.kill) anim.a.value.kill();
+//       gsap.set(target, { autoAlpha: 0 });
+//     }
+//   });
 
-  return (
-    <div className={" " + " " + className} ref={ref}>
-      {children}
-    </div>
-  );
-};
+//   return (
+//     <div className={" " + " " + className} ref={ref}>
+//       {children}
+//     </div>
+//   );
+// };
