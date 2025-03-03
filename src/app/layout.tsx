f57bb23f1d52b394589b3ae17f28605d8cdbca3b
Federico Valla <38297526+vallafederico@users.noreply.github.com>
2025-03-03 11:10:36 +0100

    with lenis

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
-// import { ReactLenis } from "../libs/lenis";
+import { ReactLenis } from "../libs/lenis";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -30,12 +30,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* <ReactLenis root> */}
-        <>
-          {children}
-          <PrismicPreview repositoryName={repositoryName} />
-        </>
-        {/* </ReactLenis> */}
+        <ReactLenis root>
+          <>
+            {children}
+            <PrismicPreview repositoryName={repositoryName} />
+          </>
+        </ReactLenis>
       </body>
     </html>
   );
